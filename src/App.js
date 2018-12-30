e73bdc6bc22500269547bd9ce98f7fa180b3c1a7
Juan Baires <juancbaires@gmail.com>
2018-12-29 22:07:03 -0500

    made the project portion mobile responsive

--- src/App.js
+++ src/App.js
@@ -36,24 +36,39 @@ class App extends Component {
           <div className="project">
             <div className="project__image project--one"></div>
             <div className="project__info">
-              <h1><a rel="noopener noreferrer" target="_blank" href="https://ateball.surge.sh/">Ate-Ball</a></h1>
-              <p>My latest Project where I partnered with Luis Ortiz to create a React app that will solve the world problem risen by the question "where do you want to go eat?" Using geolocation, Zomatos and Yelp Fusion Api I present to you, Ateball. React Native version coming soon. </p>
+              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://ateball.surge.sh/">Ate-Ball</a></h1>
+              <p>My latest Project where I partnered with Luis Ortiz to create a React app that will solve the world problem risen by the question "where do you want to go eat?" Using geolocation, Zomatos and Yelp Fusion Api I present to you, Ateball.</p>
               <div className="project__buttons"><span>React</span><span>CSS</span><span>HTML</span><span>Yelp</span>
                 <span>Zomato</span><span>Geolocation</span></div>
             </div>
           </div>
           <div className="project">
-            <div className="project__image"></div>
-            <div className="project__info"></div>
+            <div className="project__image project--two"></div>
+            <div className="project__info">
+              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://sweetest-memories-portfolio.herokuapp.com/">PixaShare</a></h1>
+              <p>PixaShare is an application I made to implement the MEHN stack(MongoDB, Express, Handlebars, Node). I created it using these technologies:</p>
+              <div className="project__buttons"><span>Bootstrap</span><span>HTML</span><span>Handlebars</span><span>Express</span><span>Node</span><span>Mongo</span></div>
+            </div>
           </div>
           <div className="project">
-            <div className="project__image"></div>
-            <div className="project__info"></div>
+            <div className="project__image project--three"></div>
+            <div className="project__info">
+              <h1 className="project__title"><a href="">Sweetest-Memories</a></h1>
+              <p>This is a website I update and maintain. I created this website using a bootstrap theme from bootstrapmade for a Photographer using the following technologies:</p>
+              <div className="project__buttons"><span>Bootstrap</span><span>HTML</span><span>JavaScript</span></div>
+            </div>
           </div>
           <div className="project">
-            <div className="project__image"></div>
-            <div className="project__info"></div>
+            <div className="project__image project--four"></div>
+            <div className="project__info">
+              <h1 className="project__title"><a href="">Fron-Trivia</a></h1>
+              <p>Fron-trivia is a game I made to help test my HTML, CSS, and JavaScript knowledge using the following technologies:</p>
+              <div className="project__buttons"><span>HTML</span><span>CSS</span><span>JavaScript</span></div>
+            </div>
           </div>
+        </section>
+        <section className="about">
+          <h1>About Me</h1>
         </section>
         <main>
           <Switch>
