c2f8a5fa48d1623ee77b0478e8f70b8a3a6a2170
Juan Baires <juancbaires@gmail.com>
2019-01-04 22:18:10 -0500

    Completed the footer with copyright and social media icons and made it mobile friendly with flex box. Now I need to run through the site to make sure its bug free and deploy it to the domain I purchased

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <section className="hero">
+        <section id="hero" className="hero">
           <div className='toggle'><i onClick={this.toggleMobileMenu} className="fas fa-bars" aria-hidden="true"></i></div>
           <div style={{ backgroundColor: this.state.headerColor, position: 'fixed' }} id='nav--mobile' className={this.state.toggleMenu}>
-            <li className="nav__item">Home</li>
-            <li className="nav__item">About</li>
-            <li className="nav__item">Projects</li>
-            <li className="nav__item">Contact</li>
+            <li className="nav__item"><a href="#hero">Home</a></li>
+            <li className="nav__item"><a href="#about">About</a></li>
+            <li className="nav__item"><a href="#projects">Projects</a></li>
+            <li className="nav__item"><a href="#contact">Contact</a></li>
           </div>
           <div className="navInfo">
             <h1>Front-End Web Developer</h1>
@@ -45,7 +45,7 @@ class App extends Component {
             </div>
           </div>
         </section>
-        <section className="project__container">
+        <section id="projects" className="project__container">
           <div className="project">
             <div className="project__image project--one"></div>
             <div className="project__info">
@@ -58,7 +58,7 @@ class App extends Component {
           <div className="project">
             <div className="project__image project--two"></div>
             <div className="project__info">
-              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://sweetest-memories-portfolio.herokuapp.com/">PixaShare</a></h1>
+              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://sleepy-bayou-76332.herokuapp.com/">PixaShare</a></h1>
               <p>PixaShare is an application I made to implement the MEHN stack(MongoDB, Express, Handlebars, Node). I created it using these technologies:</p>
               <div className="project__buttons"><span>Bootstrap</span><span>HTML</span><span>Handlebars</span><span>Express</span><span>Node</span><span>Mongo</span></div>
             </div>
@@ -66,7 +66,7 @@ class App extends Component {
           <div className="project">
             <div className="project__image project--three"></div>
             <div className="project__info">
-              <h1 className="project__title"><a href="">Sweetest-Memories</a></h1>
+              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://sweetest-memories-portfolio.herokuapp.com/">Sweetest-Memories</a></h1>
               <p>This is a website I update and maintain. I created this website using a bootstrap theme from bootstrapmade for a Photographer using the following technologies:</p>
               <div className="project__buttons"><span>Bootstrap</span><span>HTML</span><span>JavaScript</span></div>
             </div>
@@ -74,13 +74,14 @@ class App extends Component {
           <div className="project">
             <div className="project__image project--four"></div>
             <div className="project__info">
-              <h1 className="project__title"><a href="">Fron-Trivia</a></h1>
+              <h1 className="project__title"><a rel="noopener noreferrer" target="_blank" href="https://juancbaires.github.io/Trivia/">Fron-Trivia</a></h1>
               <p>Fron-trivia is a game I made to help test my HTML, CSS, and JavaScript knowledge using the following technologies:</p>
               <div className="project__buttons"><span>HTML</span><span>CSS</span><span>JavaScript</span></div>
             </div>
           </div>
         </section>
         <a href="mailto:juancbaires@gmail.com?Subject=Hello" className="contact--button"><i className="fas fa-envelope"></i> Contact</a>
+        <div id="about"></div>
         <h1 className="about__header">About</h1>
         <section className="about__container">
           <div className="aboutImage"></div>
@@ -91,7 +92,7 @@ class App extends Component {
                and guided me during my Army service. I am able to adapt and overcome any problem at hand.</p>
           </div>
         </section>
-        <section className="contact">
+        <section id="contact" className="contact">
           <div className="contact__container">
             <h1 className="contact__header">Contact</h1>
           </div>
@@ -105,13 +106,14 @@ class App extends Component {
         </section>
         <footer className="socialMedia__container">
           <div className="socialMedia__items">
-            <a href="#" className="media"><i className="fab fa-linkedin"></i></a>
-            <a href="#" className="media"><i className="fab fa-github-square"></i></a>
-            <a href="#" className="media"><i className="fab fa-twitter-square"></i></a>
-            <a href="#" className="media"><i className="fab fa-youtube-square"></i></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/juan-baires-2969a5122/" className="media"><i className="fab fa-linkedin"></i></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://github.com/juancbaires/" className="media"><i className="fab fa-github-square"></i></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/juancbaires?lang=en" className="media"><i className="fab fa-twitter-square"></i></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/channel/UCtuVSLdprDObQrfrTJvoeJw?view_as=subscriber" className="media"><i className="fab fa-youtube-square"></i></a>
           </div>
           <div className="copyright">
-            <h3 className="copyright__items">Juan Baires</h3>
+            <p>&copy; All Rights Reserved</p>
+            <p>Created and Designed by Juan Baires Using React</p>
           </div>
         </footer>
       </div >
